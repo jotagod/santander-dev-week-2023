d28bc51b8885b0f2818c34b07b3af9fe329c273f
João Aurélio <joao_mcd@hotmail.com>
2024-07-28 14:33:24 -0300

    pequena modificacao feita, adicionado link as noticias

--- src/main/java/me/dio/domain/model/BaseItem.java
+++ src/main/java/me/dio/domain/model/BaseItem.java
@@ -16,12 +16,22 @@ public abstract class BaseItem {
 
     private String description;
 
+    private String link;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getLink() {
+        return link;
+    }
+
+    public void setLink(Long link) {
+        this.link = link;
     }
 
     public String getIcon() {
